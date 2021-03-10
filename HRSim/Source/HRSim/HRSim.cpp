3b73a6d1f0613fccb3c9d3a1236aac5d1978dd51
Dilan Çelebi <40766083+DilanCelebi@users.noreply.github.com>
2021-03-10 11:19:42 +0300

    Delete HRSim directory

--- HRSim/Source/HRSim/HRSim.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "HRSim.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HRSim, "HRSim" );
